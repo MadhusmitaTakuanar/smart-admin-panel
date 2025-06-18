66b2bb92d04154bc50f4ad03b67eba6f5fefb753
MadhusmitaTakuanar <madhusmitakuanar1999@gmail.com>
2025-06-18 16:19:44 +0530

    Installed Bootstrap, Angular Material, and PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ButtonModule } from 'primeng/button'; 
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
